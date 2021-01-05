a5992d24e1cd00a288524bf2c031797fc2fd488f
OlgaTar <o_v_v_a@mail.ru>
2021-01-05 20:49:42 +0300

    HelloWorld first

--- src/olga/taran/Go/HelloWorld.java
+++ src/olga/taran/Go/HelloWorld.java
@@ -3,7 +3,4 @@ package olga.taran.Go;
 public class HelloWorld {
     public static void main (String[] args)
     {System.out.println(" Привет,детка.Ты- умница! ");}
-    //public static void main (String[] args)
-    //{System.out.print(" Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");}
-
 }
